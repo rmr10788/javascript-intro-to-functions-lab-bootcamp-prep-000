4d99fb80209a4013671f15583bca7e62367f62b2
Rachel Rapoza <rachelrapoza@gmail.com>
2017-10-01 23:55:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function whisper(string) {
   return string.toLowerCase ()
 }
 
-function logShout ("hello") {
-  console.log(string.toUpperCase)
+function logShout (hello) {
+  console.log('hello'.toUpperCase)
 }
