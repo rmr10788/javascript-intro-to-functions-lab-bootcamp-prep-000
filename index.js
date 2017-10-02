e817d4ffcd79a343a305b14a25b59120588b9c3d
Rachel Rapoza <rachelrapoza@gmail.com>
2017-10-02 00:18:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ function logWhisper (hello) {
 function sayHiToGrandma (string.innerHTML) {
   if (string.toLowerCase) {
     return ("I can't hear you!")
-    
+
   } else if (string.toUpperCase) {
     return ("YES INDEED!")
-    
+
   } else if (string === "I love you, Grandma.") {
     return ("I love you, too.")
   }
